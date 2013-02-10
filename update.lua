0efd1a713652aa78d66ac0f403b2b95a8c886388
John Reeves <vectorjohn@gmail.com>
2013-02-10 05:09:45 -0800

    an updater maybe

---
+++ update.lua
@@ -0,0 +1,33 @@
+src = 'http://home.vectorjohn.com/omgturtles'
+dir = '/john'
+
+fs.mkdir( dir )
+
+print( 'loading index file...' )
+index = http.get( src .. '/index.txt' )
+if not index then
+	print( "something went wrong getting index" )
+	exit()
+end
+
+f = fs.open( dir..'/index.txt', 'w' )
+f.write( index )
+
+f.close()
+
+index = fs.open( dir..'/index.txt', 'r' )
+
+filename = index.readLine()
+while filename do
+	print( 'getting ', filename )
+	data = http.get( src..'/'..filename )
+	if not data then
+		print( 'Error loading file: ', filename )
+	else
+		f = fs.open( dir..'/'..filename, 'w' )
+		f.write( data )
+		f.close()
+	end
+
+	filename = index.readLine()
+end
