fa6e47ee82f7fe547bc8be52276b185528faf612
John Reeves <vectorjohn@gmail.com>
2013-03-10 15:26:31 -0700

    moved the queue out of dstarlite because other things might want to use it.

---
+++ PQueue.lua
@@ -0,0 +1,136 @@
+-- cmp is how we compare the keys
+-- TODO: a nicety of this would be if all functions returning a value
+--      returned their key also
+function PQueue( cmp )
+    local self = {}
+    local nodemap = {}
+    local heap = {}
+    local next = 1
+
+    function swap( i1, i2 )
+        heap[ i1 ].idx, heap[ i2 ].idx = i2, i1
+        heap[ i1 ], heap[ i2 ] = heap[ i2 ], heap[ i1 ]
+    end
+
+    function percolateDown( i )
+        local li, ri = 2 * i, 2 * i + 1
+        local left, right = heap[ li ], heap[ ri ]
+        local cur = heap[ i ]
+        local smallest = i
+
+        if li < next and cmp( left.key, heap[ smallest ].key ) < 0 then
+            smallest = li
+        end
+
+        if ri < next and cmp( right.key, heap[ smallest ].key ) < 0 then
+            smallest = ri
+        end
+
+        if smallest ~= i then
+            swap( i, smallest )
+            percolateDown( smallest )
+        end
+    end
+
+    function percolateUp( i )
+        if i >= next then return end
+
+        local pi = math.floor( i / 2 )
+        local key = heap[ i ].key
+        while pi > 0 do
+            if cmp( key, heap[ pi ].key ) < 0 then
+                swap( pi, i )
+                i, pi = pi, math.floor( pi / 2 )
+            else
+                return
+            end
+        end
+    end
+
+    self = {
+
+        nodes = function()
+            local i = 1
+            return function()
+                local v = heap[ i ]
+                if v == nil then return nil end
+                i = i + 1
+                return v.key, v.value
+            end
+        end,
+
+        insert = function( val, key )
+            local node = {key = key, value = val, idx = next}
+            nodemap[ val ] = node
+
+            heap[ next ] = node
+            next = next + 1
+            percolateUp( next - 1 )
+
+        end,
+
+        -- TODO: replace this with percolation
+        update = function( node, key )
+            if self.remove( node ) then
+                self.insert( node, key )
+            end
+        end,
+
+        remove = function ( val )
+
+            if next == 1 then return nil end
+            if nodemap[ val ] == nil then return nil end
+            local node = nodemap[ val ]
+            nodemap[ val ] = nil
+
+            next = next - 1
+            if next == 1 then
+                heap[ next ] = nil
+                return val
+            end
+
+            local i = node.idx -- gets changed by swap
+            swap( node.idx, next )
+            heap[ next ] = nil
+
+            percolateUp( i )
+            percolateDown( i )
+
+            return val
+        end,
+
+        top = function()
+            return heap[1] and heap[1].value
+        end,
+
+        getKey = function( val )
+            local node = nodemap[ val ]
+            if node then
+                return node.key
+            end
+            return nil
+        end,
+
+        topKey = function()
+            return heap[1] and heap[1].key
+        end,
+
+        pop = function()
+            if heap[1] then
+                return self.remove( heap[1].value )
+            end
+            return nil
+        end,
+
+        contains = function( node )
+            return nodemap[ node ] ~= nil
+        end,
+
+        length = function()
+            return next - 1
+        end,
+    }
+
+    return self
+end
+
--- dstarlite.lua
+++ dstarlite.lua
@@ -3,6 +3,7 @@ verbose = {}    --this is a map of features to be verbose about
 mode = 'production' -- production or development
 
 if mode == 'development' then
+    require( 'PQueue' )
     require( 'debugger' )
     require( 'profiler' )
     require "socket"
@@ -18,6 +19,7 @@ if mode == 'development' then
     end}
     
 else
+    cc_include( 'PQueue' )
     function pause() end
     profiler = {start = function() end, stop = function() end}
     socket = {gettime = function() return 0 end}
@@ -172,148 +174,6 @@ end
 
 --/SHIM
 
-
--- cmp is how we compare the keys
--- TODO: a nicety of this would be if all functions returning a value
---      returned their key also
-function DSLPQueue( cmp )
-    local self = {}
-    local nodemap = {}
-    local heap = {}
-    local next = 1
-
-    function swap( i1, i2 )
-        heap[ i1 ].idx, heap[ i2 ].idx = i2, i1
-        heap[ i1 ], heap[ i2 ] = heap[ i2 ], heap[ i1 ]
-        --if next > i1 and next > i2 then
-        --end
-    end
-
-    function percolateDown( i )
-        local li, ri = 2 * i, 2 * i + 1
-        local left, right = heap[ li ], heap[ ri ]
-        local cur = heap[ i ]
-        local smallest = i
-
-        if li < next and cmp( left.key, heap[ smallest ].key ) < 0 then
-            smallest = li
-        end
-
-        if ri < next and cmp( right.key, heap[ smallest ].key ) < 0 then
-            smallest = ri
-        end
-
-        if smallest ~= i then
-            swap( i, smallest )
-            percolateDown( smallest )
-        end
-    end
-
-    function percolateUp( i )
-        if i >= next then return end
-
-        local pi = math.floor( i / 2 )
-        local key = heap[ i ].key
-        while pi > 0 do
-            if cmp( key, heap[ pi ].key ) < 0 then
-                swap( pi, i )
-                i, pi = pi, math.floor( pi / 2 )
-            else
-                return
-            end
-        end
-    end
-
-    self = {
-
-        nodes = function()
-            local i = 1
-            return function()
-                local v = heap[ i ]
-                if v == nil then return nil end
-                i = i + 1
-                return v.key, v.value
-            end
-        end,
-
-        insert = function( val, key )
-            self.stats.maxl = math.max( self.stats.maxl, next )
-
-            local now = socket.gettime()
-            self.stats.insert = self.stats.insert + 1
-
-            local node = {key = key, value = val, idx = next}
-            nodemap[ val ] = node
-
-            heap[ next ] = node
-            next = next + 1
-            percolateUp( next - 1 )
-
-            updateTime = updateTime + ( socket.gettime() - now )
-        end,
-
-        -- TODO: replace this with percolation
-        update = function( node, key )
-            self.stats.update = self.stats.update + 1
-            if self.remove( node ) then
-                self.insert( node, key )
-            end
-        end,
-
-        remove = function ( val )
-            self.stats.remove = self.stats.remove + 1
-
-            if next == 1 then return nil end
-            if nodemap[ val ] == nil then return nil end
-            local node = nodemap[ val ]
-            nodemap[ val ] = nil
-
-            next = next - 1
-            if next == 1 then
-                heap[ next ] = nil
-                return val
-            end
-            local now = socket.gettime()
-
-            local i = node.idx -- gets changed by swap
-            swap( node.idx, next )
-            heap[ next ] = nil
-
-            percolateUp( i )
-            percolateDown( i )
-
-            removeTime = removeTime + ( socket.gettime() - now )
-            return val
-        end,
-
-        top = function()
-            return heap[1] and heap[1].value
-        end,
-
-        topKey = function()
-            return heap[1] and heap[1].key
-        end,
-
-        pop = function()
-            if heap[1] then
-                return self.remove( heap[1].value )
-            end
-            return nil
-        end,
-
-        stats = {  update = 0, insert = 0, remove = 0, maxl = 0},
-
-        contains = function( node )
-            return nodemap[ node ] ~= nil
-        end,
-
-        length = function()
-            return next - 1
-        end,
-    }
-
-    return self
-end
 
 function NodeMap()
 	local nodes = {}
@@ -663,7 +523,7 @@ function DStarLite( start, goal, map, onmove )
             return kd1
         end
 
-        U = DSLPQueue( CompareKey )
+        U = PQueue( CompareKey )
 
         U.insert( start, CalculateKey( start ) )
         map.add( goal.v, goal )
@@ -910,6 +770,6 @@ if mode == 'development' then
 
     DStarLite( s, g, nil )
 
-    dprint( 'Total time in DSLPQueue.insert: ', updateTime )
-    dprint( 'Total time in DSLPQueue.remove: ', removeTime )
+    dprint( 'Total time in PQueue.insert: ', updateTime )
+    dprint( 'Total time in PQueue.remove: ', removeTime )
 end
